fc5e44ef48b3edb654e7e870a59e30db729d8bdc
Lukas Stejskal <lucastej@gmail.com>
2011-08-25 16:03:16 +0200

    Added example for creating campaign with basic data only

---
+++ examples/add_basic_campaign.rb
@@ -0,0 +1,25 @@
+
+require 'adapi'
+
+# add campaign with basic data only
+
+campaign_data = {
+  :name => "Ataxo Campaign #%d" % (Time.new.to_f * 1000).to_i,
+  :status => 'PAUSED',
+  :bidding_strategy => { :xsi_type => 'ManualCPC' },
+  :budget => {
+    :period => 'DAILY',
+    :amount => { :micro_amount => 50000000 },
+    :delivery_method => 'STANDARD'
+  },
+
+  # Set the campaign network options to Search and Search Network.
+  :network_setting => {
+    :target_google_search => true,
+    :target_search_network => true,
+    :target_content_network => false,
+    :target_content_contextual => false
+  }
+}
+ 
+p Adapi::Campaign.new(:data => campaign_data).create
