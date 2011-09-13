8e6e06a339356daaba6b7cbdbe3e9cf5856ac4ac
Lukas Stejskal <lucastej@gmail.com>
2011-09-13 13:54:21 +0200

    [HOTFIX] disable ssl authentication for curb in HTTPI

--- lib/httpi_request_monkeypatch.rb
+++ lib/httpi_request_monkeypatch.rb
@@ -1,5 +1,6 @@
 
 # manually hardcode timeouts for HTTPI to 5 minutes (300 seconds)
+# HOTFIX there's no way how to do it properly through HTTPI
 
 module HTTPI
   class Request
@@ -9,6 +10,26 @@ module HTTPI
 
     def read_timeout
       300
+    end
+  end
+end
+
+# disable ssl authentication in curb
+# HOTFIX for bug in HTTPI
+
+module HTTPI
+  module Adapter
+    class Curb
+
+    private
+
+      def setup_client(request)
+        basic_setup request
+        setup_http_auth request if request.auth.http?
+        # setup_ssl_auth request.auth.ssl if request.auth.ssl?
+        # setup_ntlm_auth request if request.auth.ntlm?
+      end
+      
     end
   end
 end
