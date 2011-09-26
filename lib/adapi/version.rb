dad49e164a2f27a0a5067d4f3aa786c47a8077a5
Lukas Stejskal <lucastej@gmail.com>
2011-09-26 11:08:44 +0200

    Version bump 0.0.3

--- lib/adapi/version.rb
+++ lib/adapi/version.rb
@@ -1,7 +1,18 @@
 module Adapi
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 
   # CHANGELOG:
+  #
+  # 0.0.3
+  # converted to ActiveModel
+  # moved common functionality to Api class
+  # changed http client to curb and hotfix ssl authentication bug in HTTPI
+  # added basic error handling
+  # changed DSL for Campaign attributes
+  # changed Ad model to general Ad model and moved TextAd to separate model
+  # added support for more target types and changed DSL for CampaignTarget
+  # converted to Ruby 1.9.2 (should work in Ruby 1.8.7 as well)
+  #
   # 0.0.2
   # [FIX] switched google gem dependencies from edge to stable release
 end
