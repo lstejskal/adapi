f53b7c0afcf4c1b60573a16d3a853cc646f8dd5f
Lukas Stejskal <lucastej@gmail.com>
2011-11-10 16:23:20 +0100

    Added example: find all ad groups for campaign

---
+++ examples/find_campaign_ad_groups.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+require 'adapi'
+
+# create campaign
+require File.join(File.dirname(__FILE__), 'add_bare_ad_group')
+
+# find all ad_groups for campaign
+ 
+$ad_groups = Adapi::AdGroup.find :all, :campaign_id => $campaign[:id]
+
+p "Found %s ad groups." % $ad_groups.size
+
+$ad_groups.each do |ad_group|
+  p "ID: %s, NAME %s, STATUS %s" % [ ad_group[:id], ad_group[:name], ad_group[:status] ]
+end
