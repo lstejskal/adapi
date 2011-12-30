d67cdb44de2437fb1b2b5f2e409ae467691e7859
Lukas Stejskal <lucastej@gmail.com>
2011-12-30 17:28:47 +0100

    [CAMPAIGN CRITERIA] changed :language criteria to new format

--- test/unit/campaign_criterion_test.rb
+++ test/unit/campaign_criterion_test.rb
@@ -3,22 +3,22 @@
 require 'test_helper'
 
 module Adapi
-  class CampaignTargetTest < Test::Unit::TestCase
+  class CampaignCriterionTest < Test::Unit::TestCase
   
-    context "self.create_targets" do
+    context "self.create_criterion" do
 
-      should "parse :language targets" do
-        assert_equal CampaignTarget.create_targets(:language, ['en', 'cs']),
-          [{:language_code => 'en'}, {:language_code => 'cs'}]
+      should "parse :language criterion to Google format" do
+        assert_equal CampaignCriterion.create_criterion(:language, 'en'),
+          {:xsi_type => 'Language', :id => 1000}
       end
 
-      should "automatically convert :language targets to lowercase" do
-        assert_equal CampaignTarget.create_targets(:language, ['EN', 'CS']),
-          [{:language_code => 'en'}, {:language_code => 'cs'}]
+      should "automatically convert :language value to lowercase" do
+        assert_equal CampaignCriterion.create_criterion(:language, 'CS'),
+          {:xsi_type => 'Language', :id => 1021}
       end
 
       should "parse :geo / :country and :province targets" do
-        assert_equal CampaignTarget.create_targets(:geo, {:country => 'CZ', :province => 'CZ-PR'}),
+        assert_equal CampaignCriterion.create_criterion(:geo, {:country => 'CZ', :province => 'CZ-PR'}),
           [
             {:xsi_type => 'CountryTarget', :excluded => false, :country_code => 'CZ'},
             {:xsi_type => 'ProvinceTarget', :excluded => false, :province_code => 'CZ-PR'}
@@ -26,7 +26,7 @@ module Adapi
       end
 
       should "automatically convert :geo / :country and :province targets to uppercase" do
-        assert_equal CampaignTarget.create_targets(:geo, {:country => 'cz', :province => 'cz-pr'}),
+        assert_equal CampaignCriterion.create_criterion(:geo, {:country => 'cz', :province => 'cz-pr'}),
           [
             {:xsi_type => 'CountryTarget', :excluded => false, :country_code => 'CZ'},
             {:xsi_type => 'ProvinceTarget', :excluded => false, :province_code => 'CZ-PR'}
@@ -34,7 +34,7 @@ module Adapi
       end
 
       should "parse :geo / :proximity targets" do
-        assert_equal CampaignTarget.create_targets(:geo,
+        assert_equal CampaignCriterion.create_criterion(:geo,
           {:proximity => {:geo_point => '38.89859,-77.035971', :radius => '10 km'}}),
           [{ :xsi_type => 'ProximityTarget', :excluded => false,
             :radius_in_units => 10, :radius_distance_units => 'KILOMETERS',
