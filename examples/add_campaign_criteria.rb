c2d18249c3500ccd25b509e1e30e8c5a68c35cd2
Lukas Stejskal <lucastej@gmail.com>
2011-12-31 23:24:07 +0100

    [CAMPAIGN CRITERIA] added criterium: location.id

--- examples/add_campaign_criteria.rb
+++ examples/add_campaign_criteria.rb
@@ -8,15 +8,17 @@ require_relative 'add_bare_campaign'
 $campaign_criterion = Adapi::CampaignCriterion.new(
   :campaign_id => $campaign[:id],
   :criteria => {
-    :language => [ 'en', 'cs' ]
-=begin
-    :geo => {
+    :language => [ 'en' ],
+    :location => {
+      # core location types
+      :id => 21137
+      # :proximity => { :geo_point => '50.083333,14.366667', :radius => '50 km' }
+
+      # interpreted location types
       # :country => 'CZ'
       # :province => 'CZ-PR'
       # :city => { :city_name => 'Prague', :province_code => 'CZ-PR', :country_code => 'CZ' }
-      :proximity => { :geo_point => '50.083333,14.366667', :radius => '50 km' }
     }
-=end
   }
 )
 
