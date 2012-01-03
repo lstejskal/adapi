4eef493857ec0dcb581e88f87c51668c81a97d5d
Lukas Stejskal <lucastej@gmail.com>
2012-01-03 10:32:06 +0100

    [REFACTOR] ruby version check

--- lib/adapi.rb
+++ lib/adapi.rb
@@ -6,7 +6,7 @@ require 'yaml'
 require 'pp'
 
 require 'active_model'
-# require only ActiveSupport parts that we actually use
+# TODO require only ActiveSupport parts that we actually use
 require 'active_support/all'
 
 require 'adapi/version'
@@ -20,20 +20,16 @@ require 'adapi/keyword'
 require 'adapi/ad'
 require 'adapi/ad/text_ad'
 
-# monkeypatch HTTPI
+# monkeypatch HTTPI - important, check the file!
 require 'httpi_request_monkeypatch'
 
 HTTPI.adapter = :curb
-# supress HTTPI output
-# HTTPI.log = false
+HTTPI.log = false # supress HTTPI output
 
 module Adapi
   API_VERSION = :v201101  
 end
 
-# check ruby version, should be 1.9
-# FIXME there's gotta be more elegant way to do this
-`ruby -v`.to_s =~ /^ruby (\d\.\d)\./
-if $1.to_f < 1.9
+if RUBY_VERSION < '1.9'
   puts "WARNING: please use ruby 1.9, adapi gem won't work properly in 1.8 and earlier versions"
 end
