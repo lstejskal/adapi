028c1a82d6f0fae3b70530240d90cb8893658ed0
Johannes Kaefer <jak4@lieserl.(none)>
2012-01-03 19:53:17 +0100

    Added AdParamService.create method

--- lib/adapi/ad_param.rb
+++ lib/adapi/ad_param.rb
@@ -4,58 +4,53 @@ module Adapi
   class AdParam < Api
     attr_accessor :ad_group_id, :criterion_id, :insertion_text, :param_index
 
-    #validates_presence_of :ad_group_id
+    validates_presence_of :ad_group_id, :criterion_id
 
     def attributes
-
+      super.merge(
+        'ad_group_id' => ad_group_id, 'criterion_id' => criterion_id,
+        'insertion_text' => insertion_text, 'param_index' => param_index
+      )
     end
 
     def initialize(params = {})
       params[:service_name] = :AdParamService
 
-      %w{ ad_group_id criterion_id }.each do |param_name|
+      %w{ ad_group_id criterion_id insertion_text param_index }.each do |param_name|
         self.send "#{param_name}=", params[param_name.to_sym]
       end
 
       super(params)
     end
 
-    # ad-specific mutate wrapper, deals with PolicyViolations for ads
-    #
-    def mutate(operation)
-      operation = [operation] unless operation.is_a?(Array)
-
-      # fix to save space during specifyng operations
-      operation = operation.map do |op|
-        op[:operand].delete(:status) if op[:operand][:status].nil?
-        op
-      end
+    def create
+      operation = {
+        :operator => 'SET',
+        :operand => serializable_hash
+      }
 
       begin
-        response = @service.mutate(operation)
+        response = @service.mutate([operation])
 
-      rescue AdsCommon::Errors::HttpError => e
-        self.errors.add(:base, e.message)
+      #rescue AdsCommon::Errors::HttpError => e
+        #self.errors.add(:base, e.message)
 
-      # traps any exceptions raised by AdWords API
-      rescue AdwordsApi::Errors::ApiException => e
-        # return PolicyViolations so they can be sent again
-        e.errors.each do |error|
-          if (error[:api_error_type] == 'PolicyViolationError') && error[:is_exemptable]
-            self.errors.add(error[:api_error_type], error[:key])
-          else
-            # otherwise, just report the errors
-            self.errors.add( "[#{self.xsi_type.underscore}]", "#{error[:error_string]} @ #{error[:field_path]}")
-          end
-        end
+      ## traps any exceptions raised by AdWords API
+      #rescue AdwordsApi::Errors::ApiException => e
+      #  # return PolicyViolations so they can be sent again
+      #  e.errors.each do |error|
+      #    if (error[:api_error_type] == 'PolicyViolationError') && error[:is_exemptable]
+      #      self.errors.add(error[:api_error_type], error[:key])
+      #    else
+      #      # otherwise, just report the errors
+      #      self.errors.add( "[#{self.xsi_type.underscore}]", "#{error[:error_string]} @ #{error[:field_path]}")
+      #    end
+      #  end
       end
 
       response
     end
 
-    # if nothing else than single number or string at the input, assume it's an
-    # id and we want to find campaign by id
-    #
     def self.find(params = {})
       params.symbolize_keys!
 
@@ -68,7 +63,6 @@ module Adapi
 
       selector = {
         :fields => ['AdGroupId', 'CriterionId', 'InsertionText', 'ParamIndex'],
-        :ordering => [{:field => 'AdGroupId', :sort_order => 'ASCENDING'}],
         :predicates => predicates
       }
 
@@ -81,6 +75,15 @@ module Adapi
       end
 
       response
+    end
+
+    def serializable_hash
+      {
+        :ad_group_id    => ad_group_id,
+        :criterion_id   => criterion_id,
+        :param_index    => param_index,
+        :insertion_text => insertion_text
+      }
     end
   end
 end
