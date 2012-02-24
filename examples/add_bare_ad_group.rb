706a7981c4f06a0a17e79e5623878eeae381bb93
Lukas Stejskal <lucastej@gmail.com>
2012-02-24 22:56:13 +0100

    [FIX] parsing of AdGroup bid amounts

--- examples/add_bare_ad_group.rb
+++ examples/add_bare_ad_group.rb
@@ -12,16 +12,13 @@ $ad_group_data = {
   :campaign_id => $campaign[:id],
   :name => "AdGroup #%d" % (Time.new.to_f * 1000).to_i,
   :status => 'ENABLED',
-  # TODO refactor AdGroup.bids DSL
   :bids => {
-    # this should be set automatically, it's dependent on Campaign.bids
     :xsi_type => 'BudgetOptimizerAdGroupBids',
     :proxy_keyword_max_cpc => 10
   }
 }
  
 $ad_group = Adapi::AdGroup.create($ad_group_data)
-
 
 p "Created ad_group ID #{$ad_group.id} for campaign ID #{$ad_group.campaign_id}"
 p $ad_group.attributes
