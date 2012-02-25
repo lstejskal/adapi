3d2c278b808e83bad9625563ac1937764c4478fc
Lukas Stejskal <lucastej@gmail.com>
2012-02-25 22:44:29 +0100

    Reverted Keyword model to earlier version, fix predicates parsing in find method

--- lib/adapi/keyword.rb
+++ lib/adapi/keyword.rb
@@ -17,54 +17,26 @@
 
 module Adapi
   class Keyword < AdGroupCriterion
-    #:criterion=>{:id=>105790288, :type=>"KEYWORD", :criterion_type=>"Keyword", :text=>"systemtelefon", :match_type=>"PHRASE", :xsi_type=>"Keyword"}
-    attr_accessor :criterion_id, :criterion_type, :criterion_type, :criterion_text, :criterion_match_type, :criterion_xsi_type
-    #:ad_group_criterion_type=>"BiddableAdGroupCriterion",
-    attr_accessor :ad_group_criterion_type
-    #:stats=>{:network=>"SEARCH", :stats_type=>"Stats"},
-    attr_accessor :stats_network, :stats_type
-    #=> {:text=>{:ad_group_id=>1658215692, :criterion_use=>"BIDDABLE", :ad_group_criterion_type=>"BiddableAdGroupCriterion",  :xsi_type=>"BiddableAdGroupCriterion"}, :match_type=>"BROAD", :negative=>false}
-    attr_accessor :criterion_use, :ad_group_criterion_type, :xsi_type, :negative #:match_type
+
+    attr_accessor :keywords
 
     def attributes
-      super.merge(serializable_hash)
+      super.merge('keywords' => keywords)
     end
 
-    def initialize( ext_options = {} )
-      options       = default_options.merge(ext_options.delete_if{|k,v|v.nil?})
+    def initialize(params = {})
+      params[:service_name] = :AdGroupCriterionService
 
-      # I think this is just not necessary and maybe restricting later on
-      #%w{ keywords }.each do |param_name|
-      #  self.send "#{param_name}=", params[param_name.to_sym]
-      #end
+      @xsi_type = 'AdGroupCriterion'
 
-      # FIXME to be replaced with loop
+      %w{ keywords }.each do |param_name|
+        self.send "#{param_name}=", params[param_name.to_sym]
+      end
 
-      #self.match_type                     = options[:match_type],
-      self.negative                       = options[:negative],
-      self.ad_group_id                    = options[:ad_group_id],
-      self.criterion_use                  = options[:criterion_use],
-      self.ad_group_criterion_type        = options[:ad_group_criterion_type],
-      self.xsi_type                       = options[:xsi_type],
-      self.id                             = options[:id],
-      self.criterion_type                 = options[:type],
-      self.criterion_text                 = options[:text],
-      self.criterion_match_type           = options[:match_type],
-      self.criterion_xsi_type             = options[:xsi_type],
-      self.stats_network                  = options[:network],
-      self.stats_type                     = options[:stats_type]
+      self.keywords ||= []
+      self.keywords.map! { |k| Keyword.keyword_attributes(k) }
 
-      #self.keywords ||= []
-      #self.keywords.map! { |k| Keyword.keyword_attributes(k) }
-
-      super(options)
-    end
-
-    def default_options
-      {
-        :service_name   => :AdGroupCriterionService,
-        :xsi_type       => 'AdGroupCriterion'
-      }
+      super(params)
     end
 
     # Converts keyword specification from shortened form to Google format
@@ -93,21 +65,22 @@ module Adapi
       { :text => keyword, :match_type => match_type, :negative => negative }
     end
 
+
     def create
-      operations = [
+      operations = @keywords.map do |keyword|
         {
           :operator => 'ADD', 
           :operand => {
-            :xsi_type => (negative ? 'NegativeAdGroupCriterion' : 'BiddableAdGroupCriterion'),
-            :ad_group_id => ad_group_id,
+            :xsi_type => (keyword[:negative] ? 'NegativeAdGroupCriterion' : 'BiddableAdGroupCriterion'),
+            :ad_group_id => @ad_group_id,
             :criterion => {
               :xsi_type => 'Keyword',
-              :text => criterion_text,
-              :match_type => criterion_match_type
+              :text => keyword[:text],
+              :match_type => keyword[:match_type]
             }
           }
         }
-      ]
+      end
 
       response = self.mutate(operations)
 
@@ -118,72 +91,59 @@ module Adapi
       true
     end
 
-    def serializable_hash
-      {
-       # :match_type                     => match_type,
-        :negative                       => negative,
-        :ad_group_id                    => ad_group_id,
-        :criterion_use                  => criterion_use,
-        :ad_group_criterion_type        => ad_group_criterion_type,
-        :xsi_type                       => xsi_type,
-        :id                             => id,
-        :criterion_type                 => type,
-        :criterion_text                 => text,
-        :criterion_match_type           => match_type,
-        :criterion_xsi_type             => xsi_type,
-        :stats_network                  => network,
-        :stats_type                     => stats_type
-      }
-    end
-
-    def self.find(ext_options = {})
-      options       = default_find_options.merge(ext_options.delete_if{|k,v|v.nil?})
+    def self.find(amount = :all, params = {})
+      params[:format] ||= :google # default, don't do anything with the data from google
+      
+      params.symbolize_keys!
+      # this has no effect, it's here just to have the same interface everywhere
+      first_only = (amount.to_sym == :first)
 
       # we need ad_group_id
-      raise ArgumentError, "AdGroup ID is required" unless options[:ad_group_id]
+      raise ArgumentError, "AdGroup ID is required" unless params[:ad_group_id]
  
-      # supported condition parameters: ad_group_id and id
-      predicates = [ :ad_group_id, :criterion_use ].map do |param_name|
-        if ext_options[param_name]
-          value = Array.try_convert(ext_options[param_name]) ? params_param_name : [options[param_name]]
-          {:field => param_name.to_s.camelcase, :operator => 'IN', :values => value }
+      # basic predicates for searching keywords - both are required
+      predicates = [
+        { :field => 'CriteriaType', :operator => 'EQUALS', :values => [ 'KEYWORD' ] },
+        { :field => 'AdGroupId', :operator => 'EQUALS', :values => [ params[:ad_group_id] ] }
+      ]
+      # supported parameters: id
+      predicates += [ :id ].map do |param_name|
+        if params[param_name]
+          {:field => param_name.to_s.camelcase, :operator => 'EQUALS', :values => params[param_name] }
         end
       end.compact
 
       # Get all the criteria for this ad group.
       selector = {
-        :fields => ['Id', 'Text'],
+        :fields => ['Id', 'CriteriaType', 'KeywordText'],
         :ordering => [{ :field => 'AdGroupId', :sort_order => 'ASCENDING' }],
-        :predicates => predicates
+        :predicates => predicates,
+        :paging => {
+          :start_index => 0,
+          :number_results => 10
+        }
       }
 
       response = Keyword.new.service.get(selector)
 
       response = (response and response[:entries]) ? response[:entries] : []
 
-      #response = case options[:format].to_sym
-      #  when :short
-      #    Keyword.shortened(response)
-      #  when :params
-      #    Keyword.parameterized(response)
-      #  else
-      #    response
-      #end
-
-      keywords = []
-
-      response.each do |r|
-        keywords << create_from_api(r)
+# for now, always return keywords in :google format
+=begin
+      response = case params[:format].to_sym
+      when :short
+        Keyword.shortened(response)
+      when :params
+        Keyword.parameterized(response)
+      else
+        response
       end
+=end
 
-      keywords
-    end
-    
-    def self.default_find_options
-      {
-        :format   => :google,
-        :amount   => :all
-      }
+      Keyword.new(
+        :ad_group_id => params[:ad_group_id],
+        :keywords => response
+      )
     end
 
     # PS: create a better UI for this?
@@ -222,27 +182,5 @@ module Adapi
       end
     end
 
-    def self.create_from_api( entry )
-      c     = entry[:criterion]
-      s     = entry[:stats]
-
-      Keyword.new(
-        #:match_type                     => entry[:match_type],
-        :negative                       => (entry[:xsi_type].eql?("NegativeAdGroupCriterion")),
-        :ad_group_id                    => entry[:ad_group_id],
-        :criterion_use                  => entry[:criterion_use],
-        :ad_group_criterion_type        => entry[:ad_group_criterion_type],
-        :xsi_type                       => entry[:xsi_type],
-
-        :id                             => c[:id],
-        :criterion_type                 => c[:type],
-        :criterion_text                 => c[:text],
-        :criterion_match_type           => c[:match_type],
-        :criterion_xsi_type             => c[:xsi_type],
-
-        :stats_network                  => s[:network],
-        :stats_type                     => s[:stats_type]
-      )
-    end
   end
 end
