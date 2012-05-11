c7ac8d7ee038ed9003a61818178d7aea3e9f3b3c
Lukas Stejskal <lucastej@gmail.com>
2012-05-11 22:00:53 +0200

    Removed obsolete commented-out curb monkeypatch

--- lib/httpi_request_monkeypatch.rb
+++ lib/httpi_request_monkeypatch.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 # manually hardcode timeouts for HTTPI to 5 minutes (300 seconds)
-# HOTFIX there's no way how to do it properly through HTTPI
+# TODO check if there's still no way to do it properly through HTTPI
+# TODO enable user to set timeout in adapi configuration
 
 module HTTPI
   class Request
@@ -14,33 +15,3 @@ module HTTPI
     end
   end
 end
-
-# according to https://github.com/taf2/curb/issues/96, should be fixed in
-# curb 0.8.0, so let's to comment it out
-=begin
-
-# disable ssl authentication in curb
-# HOTFIX for bug in HTTPI
-
-module HTTPI
-  module Adapter
-    class Curb
-
-    private
-
-      def setup_client(request)
-        basic_setup request
-        setup_http_auth request if request.auth.http?
-        # setup_ssl_auth request.auth.ssl if request.auth.ssl?
-        # setup_ntlm_auth request if request.auth.ntlm?
-
-        # HOTFIX for bug in curb 0.7.16, see issue:
-        # https://github.com/taf2/curb/issues/96
-        client.resolve_mode = :ipv4
-      end
-      
-    end
-  end
-end
-
-=end
