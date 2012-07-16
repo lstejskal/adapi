8385c91a7730c0e9b8f7dea2b6af328963b16556
Lukas Stejskal <lucastej@gmail.com>
2012-07-16 14:44:03 +0200

    Added example: update_text_ad

---
+++ examples/update_text_ad.rb
@@ -0,0 +1,18 @@
+
+require 'adapi'
+
+require_relative 'add_text_ad'
+
+$new_ad = $ad.update(
+  :status => 'ACTIVE',
+  :headline => "Code like Trinity",
+  :description1 => 'Need mad update skills?',
+  :description2 => 'Check out my updates!',
+  :url => 'http://www.demupdatez.com',
+  :display_url => 'http://www.demupdatez.com'
+)
+
+$new_ad = Adapi::Ad::TextAd.find(:first, :ad_group_id => $new_ad.ad_group_id, :id => $new_ad.id )
+
+puts "\nUPDATED (AND RELOADED) AD:"
+pp $new_ad.attributes
