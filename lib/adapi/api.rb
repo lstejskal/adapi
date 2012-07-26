3a99c73778c0df91d97fe9adc218ba1e9b6ad487
Lukas Stejskal <lucastej@gmail.com>
2012-07-26 16:40:01 +0200

    [FIX] default attributes in Api#check_for_errors

--- lib/adapi/api.rb
+++ lib/adapi/api.rb
@@ -157,7 +157,7 @@ module Adapi
     # raise_errors (default: false) - raises exception CampaignError (after optional saving errors)
     #
     def check_for_errors(adapi_instance, options = {})
-      options.merge( store_errors: true, raise_errors: false )
+      options.merge!( store_errors: true, raise_errors: false )
 
       # don't store errors in this case, because errors are already there
       # and loop in store_errors method would cause application to hang
