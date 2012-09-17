2eabb5286c1ee5df82038969ad13189797c4ec3c
Lukas Stejskal <lucastej@gmail.com>
2012-09-17 16:01:52 +0200

    Added creating bare campaigns and ad_groups to integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,4 +19,53 @@ Dir[ File.join(File.dirname(__FILE__), 'factories', '*.rb') ].each { |f| require
 
 class Test::Unit::TestCase
   FakeWeb.allow_net_connect = false
+
+  # many integration tests need to use campaign or ad group
+  # instead of creating them in every test, we do it here  
+  #
+  @@bare_campaign_id = nil
+  @@bare_ad_group_id = nil
+
+  def create_bare_campaign!(force = false)
+    if force or @@bare_campaign_id.nil?
+      campaign = Adapi::Campaign.create(
+        name: "Campaign #%d" % (Time.new.to_f * 1000).to_i,
+        status: 'PAUSED',
+        bidding_strategy: { 
+          xsi_type: 'BudgetOptimizer', 
+          bid_ceiling: 20 
+        },
+        budget: 50
+      )
+
+      if campaign.errors.any?
+        raise "CANNOT CREATE BARE CAMPAIGN (during test initialization):\n" + campaign.errors.join("\n")
+      end
+
+      @@bare_campaign_id = campaign[:id]
+    end
+
+    @@bare_campaign_id
+  end
+
+  def create_bare_ad_group!(force = false)
+    if force or @@bare_ad_group_id.nil?
+      @@bare_campaign_id ||= create_bare_campaign!(true)
+
+      ad_group = Adapi::AdGroup.create(
+        campaign_id: @@bare_campaign_id,
+        name: "AdGroup #%d" % (Time.new.to_f * 1000).to_i,
+        status: 'ENABLED'
+      )
+
+      if ad_group.errors.any?
+        raise "CANNOT CREATE BARE AD GROUP (during test initialization):\n" + ad_group.errors.join("\n")
+      end
+
+      @@bare_ad_group_id = ad_group[:id]
+    end
+
+    @@bare_ad_group_id
+  end
+
 end
