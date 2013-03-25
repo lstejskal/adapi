b697d31ff4320d88a707dab54bce77b5140cdec3
Lukas Stejskal <lucastej@gmail.com>
2013-03-26 00:09:08 +0100

    Updated example: log_to_specific_account

--- examples/log_to_specific_account.rb
+++ examples/log_to_specific_account.rb
@@ -12,7 +12,7 @@ Adapi::Config.load_settings(:in_hash => {
       :user_agent => 'Coca-Cola Adwords API Test'
     },
     :service => {
-      :environment => 'SANDBOX'
+      :environment => 'PRODUCTION'
     }
   },
  :pepsi => {
@@ -24,11 +24,15 @@ Adapi::Config.load_settings(:in_hash => {
       :user_agent => 'Pepsi Adwords API Test'
     },
     :service => {
-      :environment => 'SANDBOX'
+      :environment => 'PRODUCTION'
     }
   }
 })
 
 Adapi::Config.set(:pepsi, :client_customer_id => '555-666-7777')
+p "PEPSI"
+p Adapi::Config.read
 
+Adapi::Config.set(:coca_cola, :client_customer_id => '111-222-3333')
+p "COCA_COLA"
 p Adapi::Config.read
